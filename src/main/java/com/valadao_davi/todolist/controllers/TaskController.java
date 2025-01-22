c3cc25a21cd934fa1da395da55d97a1f8926675b
valadao-davi <davi.raichu@gmail.com>
2025-01-22 14:14:52 -0300

    feat: taskController and get all tasks

---
+++ src/main/java/com/valadao_davi/todolist/controllers/TaskController.java
@@ -0,0 +1,23 @@
+package com.valadao_davi.todolist.controllers;
+
+import com.valadao_davi.todolist.dto.TaskDTO;
+import com.valadao_davi.todolist.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<TaskDTO> returnAll(){
+        return taskService.returnAll();
+    }
+}
